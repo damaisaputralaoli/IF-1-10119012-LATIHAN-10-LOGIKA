e83b5c8615bf8f88f78a5d032214d1796ea17e1a
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-10-20 21:41:31 +0700

    Add files via upload

---
+++ Latihan10pbo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan10pbo;
+
+/**
+ *
+ * @author damai
+ */
+public class Latihan10pbo {
+
+    public static void main (String[] args){
+    int umur = 19;
+    boolean dewasa;
+    dewasa = (umur >= 17);
+    System.out.println("Status dewasa : "+  dewasa);
+    }
+    
+}
